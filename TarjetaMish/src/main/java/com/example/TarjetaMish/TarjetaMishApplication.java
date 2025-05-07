822b049f092562ace8bef46035dc1736aee139b8
Vicente Salazar <v.salazar11@ufromail.cl>
2025-05-06 20:33:22 -0400

    archivos spring

---
+++ TarjetaMish/src/main/java/com/example/TarjetaMish/TarjetaMishApplication.java
@@ -0,0 +1,13 @@
+package com.example.TarjetaMish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarjetaMishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarjetaMishApplication.class, args);
+	}
+
+}
---
+++ TarjetaMish/src/test/java/com/example/TarjetaMish/TarjetaMishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TarjetaMish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarjetaMishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
