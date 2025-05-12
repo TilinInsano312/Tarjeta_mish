9dcd1b847ec2068a4544f69e0a40c91b08db50d2
BerAxz <bastiaxd@gmail.com>
2025-05-12 19:43:53 -0400

    refactor: rename package com.example.TarjetaMish
    rename the package com.example.TarjetaMish to org.tarjetamish

--- backend/src/main/java/org/tarjetamish/TarjetaMishApplication.java
+++ backend/src/main/java/org/tarjetamish/TarjetaMishApplication.java
@@ -1,4 +1,4 @@
-package com.example.TarjetaMish;
+package org.tarjetamish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/org/tarjetamish/TarjetaMishApplicationTests.java
+++ backend/src/test/java/org/tarjetamish/TarjetaMishApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.TarjetaMish;
+package org.tarjetamish;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
