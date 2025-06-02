7ae30648efc564bd76325a96896a13ff5ac4e262
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-06-02 19:50:58 -0400

    feat: update loginRepository

--- frontend/lib/src/domain/repository/loginRepository.dart
+++ frontend/lib/src/domain/repository/loginRepository.dart
@@ -2,31 +2,49 @@ import 'dart:convert';
 import 'package:frontend/src/domain/models/login.dart';
 import 'package:http/http.dart' as http;
 
-class Loginrepository {
+class LoginRepository {
 
   final String baseUrl;
 
-  Loginrepository({required this.baseUrl});
+  LoginRepository({required this.baseUrl});
 
   Future<LoginResponse> login (String rut, int pin) async {
-    
+   try{
     final response = await http.post(
       Uri.parse('$baseUrl/login'),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'rut': rut,
-        'pin': pin,
-      }),
-    );
+        body: jsonEncode({
+          'rut': rut,
+          'pin': pin,
+        }),
+    ).timeout(const Duration(seconds: 10));
 
-  print('Response status: ${response.statusCode}');
-  print('Response body: ${response.body}');
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
 
-    if (response.statusCode == 200){
-      return LoginResponse.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('Error al iniciar sesión');
+    switch (response.statusCode) {
+      case 200:
+        return LoginResponse.fromJson(jsonDecode(response.body));
+      case 400:
+        throw Exception('Rut o pin incorrectos');
+      case 401:
+        throw Exception('Credenciales invalidas');
+      case 500:
+        throw Exception('Error de servidor');
+      default:
+        throw Exception('Error de conexion');
     }
+   } catch (e) {
+     if (e.toString().contains('SocketException')) {
+       throw Exception('Error de conexion');
+     } else if (e.toString().contains('TimeoutException')) {
+       throw Exception('Tiempo de espera agotado');
+     } else {
+       rethrow;
+     }
+   }
+
+  
 
   }
 
