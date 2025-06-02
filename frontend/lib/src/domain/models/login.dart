7327ae88e4f441cd69b9c3cd498dd47c8c360839
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-06-02 19:49:34 -0400

    fix: fix login.dart

--- frontend/lib/src/domain/models/login.dart
+++ frontend/lib/src/domain/models/login.dart
@@ -1,18 +1,17 @@
 class LoginResponse{
 
-  final bool success;
+
   final String token;
 
   LoginResponse({
-    required this.success,
     required this.token,
   });
 
   factory LoginResponse.fromJson(Map<String, dynamic> json) {
     return LoginResponse(
-      success: json['success'] as bool,
       token: json['token'] as String,
     );
   }
+
 
 }
