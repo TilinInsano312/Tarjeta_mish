eb4b2189ab4e429fc71c3a5e5d53420d7e34ac49
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-06-02 19:48:12 -0400

    feat: add appConfig

---
+++ frontend/lib/src/domain/appConfig.dart
@@ -0,0 +1,9 @@
+class Appconfig {
+
+  static const String appName = "MyApp";
+  static const String appVersion = "1.0.0";
+  static const String baseUrl = 'https://localhost:8080/api';
+  static const Duration timeoutDuration = Duration(seconds: 10);
+
+
+}
