2ffbe581f6edbb76c520ec313216c4e39b0cf2c4
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-06-07 18:01:36 -0400

    fix: rename variables

--- frontend/lib/src/domain/models/card.dart
+++ frontend/lib/src/domain/models/card.dart
@@ -2,18 +2,21 @@ class Card{
   final String cardNumber;
   final String cardCVV;
   final String expirationDate;
+  final String cardHolderName;
 
   const Card({
     required this.cardNumber,
     required this.cardCVV,
     required this.expirationDate,
+    required this.cardHolderName,
   });
 
   factory Card.fromJson(Map<String, dynamic> json) {
     return Card(
-      cardNumber: json['card_number'] as String,
-      cardCVV: json['card_cvv'] as String,
+      cardNumber: json['number'] as String,
+      cardCVV: json['cvv'] as String,
       expirationDate: json['expirationDate'] as String,
+      cardHolderName: json['cardHolderName'] as String,
     );
   }
 }
