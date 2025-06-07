ad3668e3bb9672371c4031088d35de8aa9e76f40
Vicente Salazar <v.salazar11@ufromail.cl>
2025-06-07 14:56:30 -0400

    refactor: card package

---
+++ backend/src/main/java/org/tarjetamish/card/mapper/CardConverter.java
@@ -0,0 +1,16 @@
+package org.tarjetamish.card.mapper;
+
+import org.springframework.stereotype.Component;
+import org.tarjetamish.card.dto.CardDTO;
+import org.tarjetamish.card.model.Card;
+
+@Component
+public class CardConverter implements ICardConverter {
+
+    public CardDTO toCardDTO(Card card) {
+        return new CardDTO(card.getId(), card.getNumber(), card.getCvv(), card.getExpirationDate(), card.getCardHolderName());
+    }
+    public Card toCard(CardDTO cardDTO) {
+        return new Card(cardDTO.id(), cardDTO.number(), cardDTO.cvv(), cardDTO.expirationDate(), cardDTO.cardHolderName());
+    }
+}
--- backend/src/main/java/org/tarjetamish/card/repository/impl/JdbcCardRepository.java
+++ backend/src/main/java/org/tarjetamish/card/repository/impl/JdbcCardRepository.java
@@ -2,6 +2,7 @@ package org.tarjetamish.card.repository.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.tarjetamish.card.model.Card;
 import org.tarjetamish.card.repository.CardRepository;
@@ -15,23 +16,24 @@ import java.util.Optional;
 public class JdbcCardRepository implements CardRepository {
 
     private final JdbcTemplate jdbc;
+    private final RowMapper<Card> cardRowMapper;
 
     @Override
     public List<Card> findAll() {
         String sql = "SELECT * FROM tarjeta_mish.card";
-        return jdbc.query(sql, new CardRowMapper());
+        return jdbc.query(sql, cardRowMapper);
     }
 
     @Override
     public Optional<Card> findById(Long id) {
         String sql = "SELECT * FROM tarjeta_mish.card WHERE idcard = ?";
-        return Optional.ofNullable(jdbc.queryForObject(sql, new CardRowMapper(), id));
+        return Optional.ofNullable(jdbc.queryForObject(sql, cardRowMapper, id));
     }
 
     @Override
     public Card save(Card card) {
         String sql = "INSERT INTO tarjeta_mish.card (number, cvv, expirationdate, cardholdername) VALUES (?, ?, ?, ?)";
-        return jdbc.queryForObject(sql, new CardRowMapper(), card.getNumber(), card.getCvv(), card.getExpirationDate(), card.getCardHolderName());
+        return jdbc.queryForObject(sql, cardRowMapper, card.getNumber(), card.getCvv(), card.getExpirationDate(), card.getCardHolderName());
     }
 
     @Override
