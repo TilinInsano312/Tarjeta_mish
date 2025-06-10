abd1be9ba7f7658f3571d07756aa9ef21d2a5a13
Vicente Salazar <v.salazar11@ufromail.cl>
2025-06-10 00:40:13 -0400

    feat: UserServiceTest

--- backend/src/test/java/org/tarjetamish/service/UserServiceTest.java
+++ backend/src/test/java/org/tarjetamish/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package org.tarjetamish.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.tarjetamish.user.dto.UserDTO;
 import org.tarjetamish.user.service.UserService;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +16,25 @@ public class UserServiceTest {
     @Test
     void testListUsers() {
         assertNotNull(userService.list(), "User list should not be null");
+    }
+
+    @Test
+    void testFindById() {
+        assertTrue(userService.findById(1L).isPresent(), "User with ID 1 should exist");
+    }
+    @Test
+    void testFindByRut() {
+        String rut = "123456789";
+        assertTrue(userService.findByRut(rut).isPresent(), "User with RUT " + rut + " should exist");
+    }
+    @Test
+    void testSaveUser() {
+        int result = userService.save(new UserDTO(0L,"111111111", "Test", "User", "1234"));
+        assertEquals(1, result, "User should be saved successfully");
+    }
+    @Test
+    void testDeleteUser() {
+        String rut = "111111111";
+        assertEquals(1, userService.deleteUser(rut), "User should be deleted successfully");
     }
 }
