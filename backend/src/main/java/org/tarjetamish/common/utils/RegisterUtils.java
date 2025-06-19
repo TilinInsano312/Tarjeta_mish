b2f8ca5b1d233e828ecf9edc5af1de1cf93a8328
BerAxz <bastiaxd@gmail.com>
2025-06-19 00:54:15 -0400

    feature: add class RegisterUtils
    add RegisterUtils class which generates the data to register a user

---
+++ backend/src/main/java/org/tarjetamish/common/utils/RegisterUtils.java
@@ -0,0 +1,35 @@
+package org.tarjetamish.common.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Component
+public class RegisterUtils {
+    private final SecureRandom random = new SecureRandom();
+
+    public String generateCardNumber() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            int block = random.nextInt(10_000);
+            sb.append(String.format("%04d", block));
+        }
+        return sb.toString();
+    }
+
+    public Date generateExpirateDate() {
+        LocalDate expiry = LocalDate.now().plusYears(5);
+        return Date.from(expiry.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
+
+    public int generateAccountNumber() {
+        return 100_000_000 + random.nextInt(900_000_000);
+    }
+
+    public String generateCvv() {
+        return String.format("%03d", random.nextInt(1000));
+    }
+}
